a2ce822108cbb2f2b4d6c83be40104a7691f99d5
Navneet Singh <navneets@beingzero.in>
2023-07-21 00:04:06 +0530

    my nvim config

---
+++ after/plugin/colors.lua
@@ -0,0 +1,8 @@
+
+
+function ColorMyPencil(color)
+    color = color or "rose-pine"
+    vim.cmd.colorscheme(color)
+end
+
+ColorMyPencil()
---
+++ lua/navneet/init.lua
@@ -0,0 +1,2 @@
+require("navneet.remap")
+require("navneet.set")
